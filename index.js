ea8768329568432f85d3abed8f32a6abc559300b
MatVD <vandammemathieu@hotmail.fr>
2023-04-02 09:34:35 +0200

    add some infos on index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+// ---- Formation javascript ---- //
+// ------------------------------ //
+
+
+// 01 - Les variables
+    // Tout comme vous mettez le nom d'un carton en fonction de ce qui se trouvent à l'intérieur, 
+    // de même, le nom d'une varibale doit indiquer ce qui se trouve à l'intérieur
+
+
+    // Exercice
+        // Créer deux variables : 
+            // "numberOfSeasons" et lui affecter la valeur 6
+            // "numberOfEpisodes" et assignez lui la valeur 12
+
+    
+// Modifier la valeur d'une variable
+
+
+// Les Opérateurs
+
+    // Exercice
+        // Créer deux nouvelles variables
+            // "episodeTime" avec pour valeur 45
+            // "commercialTime" avec 5
+
+        // À l'aide des deux variables, ainsi que du nombre d'épisodes et du nombre de saisons, 
+        // créez une variable nommée "totalShowTime" qui contient le temps de visionnage total de cette série
+
+// Mutabilité des variables
+
+// Les constantes
+
+    // Exercice
+
+
+// 02 - Les opérateurs
+
+// 03 - Les variables
+
+// 04 - Les variables
+
+// 05 - Les variables
+
+// 06 - Les variables
+
