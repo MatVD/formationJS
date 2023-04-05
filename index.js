e1b5585a94d35b3f8b34be63438ca8156b2503f9
MatVD <vandammemathieu@hotmail.fr>
2023-04-05 21:00:00 +0200

    add fonction and instance lesson

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     // Tout comme vous mettez le nom d'un carton en fonction de ce qui se trouvent à l'intérieur, de même, le nom d'une varibale doit indiquer ce qui se trouve à l'intérieur
 
     // Comment déclarer une variable ? Avec let, const et var
-    let car = "bmw"
+    //let car = "bmw"
 
     car = "Seat" // => on peur réaffecter un valeur à une variable existante
 
@@ -52,7 +52,7 @@
 
 // --- Les constantes
     const nombrePostParPage = 20;
-    nombrePostParPage = 30; // Retournera une erreur dans la console car on ne peut plus changer sa valeur
+    // nombrePostParPage = 30; // Retournera une erreur dans la console car on ne peut plus changer sa valeur
 
     // Exercice 
       // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-1-les-variables
@@ -76,7 +76,7 @@
   let numberFloat = 1.3
 
 // Boolean
-  let boolean = true
+  let bool = true
   let boolean2 = false
 
 // String
@@ -121,8 +121,8 @@
 
   // Comment accéder aux données d'un objet ?
 
-    console.log(myBook.title) // dot notation
-    console.log(myBook.author)
+    // console.log(myBook.title) // dot notation
+    // console.log(myBook.author)
 
     // Exercice
       // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-3-les-objets
@@ -156,15 +156,26 @@
 // ---------------------------------------- //
   // Il existe un autre type en JavaScript : le tableau (array).
 
-    //                0         1        2        3     
-    let guests = ['Franck', 'Marvin', 'John', 'Isabelle']
+    //     marque / places / km / En circulation
+    let car = ["BMW", 5, 120000, true]
 
-  // Info : l'indice d'un tableau démarre à zéro
+    //                0         1        2        3     
+    //let guests = ['Franck', 'Marvin', 'John', 'Isabelle']
+    guests[0] // => Franck
+    guests[1] // => Marvin
+    guests[2] // => John
+    guests[3] // => Isabelle
+
+
+
+    // Info : l'indice d'un tableau démarre à zéro
 
     // Exercice
     // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-5-les-tableaux
 
   
+  //                0         1        2        3     
+  let guests = ['Franck', 'Marvin', 'John', 'Isabelle']
   // Travailler sur les tableaux
     // Length
       guests.length
@@ -190,25 +201,34 @@
 
   // ELSE (SINON) revenir à la page de connexion
 
-  // C'est ce qu'on appelle une instruction conditionnelle, parce qu'elle vérifie si certaines conditions sont réunies, et réagit en conséquence.
-  let boolean3 = true;
+ 
+  // let boolean = true;
 
-  if (boolean3) {
-    alert('La condition est remplie')
-  } else {
-    alert('La condition n\'est pas remplie')
-  }
+  // if (boolean) {
+  //   alert('La condition est remplie')
+  // } else {
+  //   alert('La condition n\'est pas remplie')
+  // }
 
-  let userLoggedIn = true
-  let firstName = "Paul"
+  // let userLoggedIn = true
+  // let firstName = "Paul"
+
+
+  // const numberOfSeats = 30;
+  // const numberOfGuests = 25;
+  // if (numberOfGuests < numberOfSeats) {
+  // // autoriser plus d'invités
+  // } else {
+  // // ne pas autoriser de nouveaux invités
+  // }
 
   // expressions de comparaisons
   // < , <= , == , >= , > , !=
-  if (firstName != "Paul") {
-    alert("Utilisateur connecté")
-  } else {
-    alert(`Bonjour ${firstName}`)
-  }
+  // if (firstName != "Paul") {
+  //   alert("Utilisateur connecté")
+  // } else {
+  //   alert(`Bonjour ${firstName}`)
+  // }
 
     // Exercice 
       // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-6-les-conditions
@@ -219,41 +239,93 @@
     // haveFuel = true
     // Si la condition est remplie, aficher "Vous êtes autorisé à conduire" sinon afficher "Dommage, il va falloir attendre"
 
-    let age = 18
+    // let age = 18
 
-    if (age >= 18) {
-      alert("Vous êtes autorisé à conduire")
-    } else {
-      alert("Dommage, il va falloir attendre")
-    }
+    // if (age >= 18) {
+    //   alert("Vous êtes autorisé à conduire")
+    // } else {
+    //   alert("Dommage, il va falloir attendre")
+    // }
     
   // Les conditions multiples
 
     // Opérateurs logiques
     // ET => && 
     // OU => ||
-    // ! => NOT
+    // NOT => !
 
   let haveAcar = true
   let haveFuel = false
   let haveKeys = true
 
-  if (age >= 18 && haveAcar && haveFuel && haveKeys) {
-    alert("Vous êtes autorisé à conduire")
-  } else {
-    alert("Dommage, il va falloir attendre")
-  }
+  // if (age >= 18 && haveAcar && haveFuel && haveKeys) {
+  //   alert("Vous êtes autorisé à conduire")
+  // } else {
+  //   alert("Dommage, il va falloir attendre")
+  // }
+
+  // if (age == 18) {
+  //   alert('Tu es tout juste majeur')
+  // } else if (age >= 99) { // 102
+  //   alert('Tu ne devrais plus conduire')
+  // } else if (age > 18) { 
+  //   alert('Tu peux conduire')
+  // } else {
+  //   alert('J\'ai rien de plus à écrire')
+  // }
 
     // Exercice 
       // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-6-les-conditions
 
+
+
   // Le scope des variables (la portée des variables)
+
+  // let userLoggedIn1 = false
+  // let welcomeMessage = ""
+
+  // if (userLoggedIn1) {
+  //   welcomeMessage = "Bienvenue à nouveau"
+  // } else {
+  //   welcomeMessage = "Bienvenue nouvel utilisateur"
+  // }
+
+  // alert(welcomeMessage)
+
     // En JavaScript, les variables créées par let  ou const  ne peuvent 
     // être vues ou utilisées qu'à l'intérieur du bloc de code dans lequel elles sont déclarées.
 
 
-
   // L'instruction switch
+  // Va être utile si on veut vérifier la valeur d'une variable par rapport à une liste de valeurs attendues
+
+  // let firstUser = {
+  //   name: "Will Alexander",
+  //   age: 33,
+  //   accountLevel: "normal"
+  // };
+
+  // let secondUser = {
+  //     name: "Sarah Kate",
+  //     age: 21,
+  //     accountLevel: "premium"
+  // };
+
+
+
+  // switch (fifthUser.accountLevel) {
+  //   case 'normal':
+  //     alert('Bienvenue à toi')
+  //     break
+
+  //   case 'premium':
+  //     alert('Bienvenue à toi premium')
+  //     break
+
+  //   default: 
+  //     alert('Bienvenue nouvel utilisateur') 
+  // }
+
 
   // Résumé kahoots
 
@@ -261,8 +333,59 @@
 // ------------------------------------------ //
 
 // For
+// const numberOfpassengers = 10
+// for (let i = 0; i < numberOfpassengers; i++) {
+//   console.log(`Le passager n° ${i} vient de rentrer dans l'avion`)
+// }
+// console.log('Tout le monde à embarqué')
+
+//                        0         1       2       3          4       5
+// let listePassagers = ['Franck', 'Sylvie', 'Flo', 'Benoit', 'Claire', 'Gilou la menace']
+
+
+// For ... in => boucler sur les tableaux
+// for (let i in listePassagers) {
+//   console.log(`Le passager prénommé(e) ${listePassagers[i]} vient de rentrer dans l'avion`)
+// }
+// console.log('Tout le monde à embarqué')
+
+// For ... of => boucler sur les tableaux et les objets
+  // let listePassagers = [
+  //   {
+  //     name: "Sarah Kate",
+  //     age: 21
+  //   },
+  //   {
+  //     name: "Audrey Simon",
+  //     age: 27
+  //   },
+  //   {
+  //     name: "Audrey Peters",
+  //     age: 27
+  //   }
+  // ]
+
+// for (let passager of listePassagers) {
+//   console.log(`Le passager prénommé(e) ${passager.name} vient de rentrer dans l'avion`)
+// }
+// console.log('Tout le monde à embarqué')
 
 // While
+
+// let seatsLeft = 10;
+// let passengersStillToBoard = 8;
+// let passengersBoarded = 0;
+
+// while (seatsLeft > 0 && passengersStillToBoard > 0) {
+//     passengersBoarded++; // un passager embarque
+//     passengersStillToBoard--; // donc il y a un passager de moins à embarquer
+//     seatsLeft--; // et un siège de moins
+// }
+
+// console.log(passengersBoarded); // imprime 8, car il y a 8 passagers pour 10 sièges
+
+    // Exercice
+      // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-7-les-boucles
 
 
 // -------- 07 La gestion des erreurs ------- //
@@ -272,7 +395,65 @@
 // on distingue généralement trois types d'erreur.
 
   // - les erreurs de syntaxe
+    // Relire son code ou le faire relire par quelqu'un
+    // IDE => VS Code => surligner les erreurs de syntaxe
+
 
   // - les erreurs de logique
+    // Une valeur erronée dans une variable
+      let ageDeLaMajorite = 19
+
+      if (ageDeLaMajorite <= 18) {
+        "Vous n'êtes pas majeur"
+      }
+
+    // mélange de condition dans les instructions if
+      if (age <= 25 && age >= 50) {
+        // Fais cela
+      }
+
+    // ordre dans lequel on ecrit nos ligne de code 
+    
+
+    //On simule avoir 22 ans donc être majeur
+    // const monAge = 22;
+
+    // if(monAge > 18) {
+    //     console.log("vous êtes majeur");
+    // }else{
+    //     console.log("vous êtes mineur");
+    // }
+    
 
   // - les erreurs d'éxécution
+    // Une page qui n'existe
+      // Si jamais cette erreur apparait, comment je vais la traiter
+      try {
+        // Le code susceptibile de générer une erreur
+        // Connexion au site
+      } catch (error) {
+        // On chope l'erreur
+      }
+
+// ------------ 08 Les fonctions ------------ //
+// ------------------------------------------ //
+
+// Une fonction est un bloc de code auquel on attribu un nom. Quand vous appelez cette fonction, vous exécutez le code qu'elle contient.
+
+// Exemple
+  // Déclaration
+
+  // Exécution
+
+
+    // Exercice 
+    // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-8-les-fonctions
+
+
+// ------------ 08 Les méthodes d'instances et les proriétés ------------ //
+// ---------------------------------------------------------------------- //
+
+// Les méthodes d'instances
+
+    // Exercice
+      // https://replit.com/team/DTBMI-javascript/Formation-JS-Exercice-8-les-fonctions
